ebfe8d5b8120cfed164f6246ccd6e964acc8151a
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-04-18 19:43:15 -0300

    fixing errors

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,12 +7,11 @@ import { generateToken } from "../services/auth.services";
 
 export const register = async (req: Request, res: Response): Promise<void> => {
     const { email, password } = req.body;
-
     try {
-
+        if(!email) throw new Error("El email es obligatorio")
+        if(!password) throw new Error("La contraseña es obligatoria")
         const hashedPassword = await hashPassword(password)
         console.log(hashedPassword)
-
         const user = await prisma.create(
             {
                 data: {
@@ -21,14 +20,20 @@ export const register = async (req: Request, res: Response): Promise<void> => {
                 }
             }
         )
-
         const token = generateToken(user)
         res.status(201).json({ token })
-
-
-
-    } catch (error) {
-
+    } catch (error: any) {
+        console.log(error)
+        if(!email){
+            res.status(400).json({message:"El email es obligatorio"})
+        }
+        if(!password){
+            res.status(400).json({message:"La contraseña es obligatoria"})
+        }
+        if(error?.code === 'P2002' && error?.meta?.target?.includes('email')){
+            res.status(400).json({message: "el mail ingresado ya existe"})
+        }
         res.status(500).json({ error: "hubo un error en el registro" })
     }
+
 }
